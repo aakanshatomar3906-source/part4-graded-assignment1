6cf6d5a8b5163bfb0d760e30364268217030b33b
aakanshatomar3906-source <aakanshatomar3906@gmail.com>
2026-05-28 20:03:24 +0530

    Add staging schema and duplicate core tables
    Created a staging area and duplicated core tables for data scrubbing and repair.

--- safe_updates.sql
+++ safe_updates.sql
@@ -1,1 +1,12 @@
 
+-- Create an isolated staging area for data scrubbing and repair
+CREATE SCHEMA IF NOT EXISTS staging_repair;
+
+-- Duplicate core tables along with their imported raw datasets
+CREATE TABLE staging_repair.students AS SELECT * FROM public.students;
+CREATE TABLE staging_repair.courses AS SELECT * FROM public.courses;
+CREATE TABLE staging_repair.enrollments AS SELECT * FROM public.enrollments;
+CREATE TABLE staging_repair.contests AS SELECT * FROM public.contests;
+CREATE TABLE staging_repair.submissions AS SELECT * FROM public.submissions;
+CREATE TABLE staging_repair.test_results AS SELECT * FROM public.test_results;
+CREATE TABLE staging_repair.operation_requests AS SELECT * FROM public.operation_requests;
